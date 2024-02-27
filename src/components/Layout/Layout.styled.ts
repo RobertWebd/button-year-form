aee71519af5de6f019a4b1050e0e87218e1b7914
Robert Zakirov <ra.20.z02@mail.ru>
2024-02-27 19:30:21 +0300

    fix: highlighting all buttons that link to the pages

--- src/components/Layout/Layout.styled.ts
+++ src/components/Layout/Layout.styled.ts
@@ -24,7 +24,7 @@ export const StyledLink = styled(Link)<StyledNavLinkProps>`
   }
 
   ${({ isactive }) =>
-    isactive &&
+    isactive === 'true' &&
     `
     background-color: rgb(48, 50, 53);
   `}
